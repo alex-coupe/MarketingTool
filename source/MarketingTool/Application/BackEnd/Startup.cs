886979515a60ca1f12ccc59767ea6e2c38009cb7
Alex <acooupe@gmail.com>
2020-12-30 13:58:09 +0000

    Adding Swagger API Docs

--- source/MarketingTool/Application/BackEnd/Startup.cs
+++ source/MarketingTool/Application/BackEnd/Startup.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DataAccess.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace ApplicationLayer
 {
@@ -31,6 +25,10 @@ namespace ApplicationLayer
             services.AddDbContext<DatabaseContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), 
                 x => x.MigrationsAssembly("DataAccess")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "MarketingAppAPI", Version = "v1" });
+            });
                 
         }
 
@@ -41,6 +39,13 @@ namespace ApplicationLayer
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestService");
+            });
 
             app.UseHttpsRedirection();
 
