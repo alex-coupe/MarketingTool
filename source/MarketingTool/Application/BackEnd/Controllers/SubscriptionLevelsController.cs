3853f704789a85eee2f68e9b25f1ad373f1b12be
Alex <acooupe@gmail.com>
2020-12-30 13:58:21 +0000

    First controller added

---
+++ source/MarketingTool/Application/BackEnd/Controllers/SubscriptionLevelsController.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubscriptionLevelsController : ControllerBase
+    {
+        private readonly IRepository<SubscriptionLevel> _repository;
+
+        public SubscriptionLevelsController(IRepository<SubscriptionLevel> repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SubscriptionLevel>>> GetSubscriptionLevels()
+        {
+            var subLevels = await _repository.GetAllAsync();
+            return Ok(subLevels);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SubscriptionLevel>> GetSubscriptionLevel(int id)
+        {
+            var subLevel = await _repository.GetAsync(id);
+
+            return Ok(subLevel);
+        }
+    }
+}
