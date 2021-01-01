d96530a1af170014188e81227cd2f9ba73673d73
Alex <acooupe@gmail.com>
2021-01-01 12:47:09 +0000

    First test made and working

--- source/MarketingTool/Support/MarketingToolTests/Unit Tests/SubscriptionLevelsControllerTests.cs
+++ source/MarketingTool/Support/MarketingToolTests/Unit Tests/SubscriptionLevelsControllerTests.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using BackEnd.Controllers;
+using DataAccess.Interfaces;
+using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace MarketingToolTests.API_Tests
 {
-    class SubscriptionLevelsControllerTests
+    public class SubscriptionLevelsControllerTests
     {
+        Mock<IRepository<SubscriptionLevel>> _subscriptionLevelRepositoryMock;
+
+        public SubscriptionLevelsControllerTests()
+        {
+            _subscriptionLevelRepositoryMock = new Mock<IRepository<SubscriptionLevel>>();
+
+            _subscriptionLevelRepositoryMock.Setup(x => x.GetAllAsync()).ReturnsAsync(new List<SubscriptionLevel> { 
+            new SubscriptionLevel
+            {
+                Name = "Free",
+                Cost = 0.00M,
+                Id = 1,
+                MaxUsers = 5
+            },
+            new SubscriptionLevel
+            {
+                Name = "Pro",
+                Cost = 20.99M,
+                Id = 2,
+                MaxUsers = 20
+            }
+            });
+        }
+          
+        [Fact]
+        public async Task get_returns_all_subscription_levels()
+        {
+            SubscriptionLevelsController _controller = new SubscriptionLevelsController(_subscriptionLevelRepositoryMock.Object);
+
+            var result = await _controller.GetSubscriptionLevels();
+
+            var actionResult = Assert.IsType<OkObjectResult>(result.Result);
+            var returnValue = Assert.IsAssignableFrom<IEnumerable<SubscriptionLevel>>(actionResult.Value);
+            var level = returnValue.FirstOrDefault();
+            Assert.Equal("Free", level.Name);
+            Assert.Equal(2, returnValue.Count());
+
+        }
     }
 }
