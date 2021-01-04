cb2ae8a7d8e5a807f28a858bd9aac3789568dfb9
Alex <acooupe@gmail.com>
2021-01-04 18:37:17 +0000

    Remove the bloat from the client table for now and just keep it name and sub level id

---
+++ source/MarketingTool/Application/DataAccess/Migrations/20210104183243_RemoveClientBloat.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class RemoveClientBloat : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Clients_SubscriptionLevels_SubscriptionLevelId",
+                table: "Clients");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Clients_SubscriptionLevelId",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "AddressOne",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "AddressTwo",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "EmailAddress",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "PostalCode",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "TelephoneOne",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "TelephoneTwo",
+                table: "Clients");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AddressOne",
+                table: "Clients",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "AddressTwo",
+                table: "Clients",
+                type: "nvarchar(20)",
+                maxLength: 20,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Clients",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailAddress",
+                table: "Clients",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PostalCode",
+                table: "Clients",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "State",
+                table: "Clients",
+                type: "nvarchar(25)",
+                maxLength: 25,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelephoneOne",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelephoneTwo",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_SubscriptionLevelId",
+                table: "Clients",
+                column: "SubscriptionLevelId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Clients_SubscriptionLevels_SubscriptionLevelId",
+                table: "Clients",
+                column: "SubscriptionLevelId",
+                principalTable: "SubscriptionLevels",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
