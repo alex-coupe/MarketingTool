57d4a2e7ee1a6c4223634e214a85a371e2e440b0
Alex <acooupe@gmail.com>
2021-01-05 18:57:26 +0000

    Using typescript for frontend

---
+++ source/MarketingTool/Application/FrontEnd/ClientApp/src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Login from './pages/Login';
+
+function App()  {
+  
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <Login />
+        </div>
+    );
+  
+}
+
+export default App;
---
+++ source/MarketingTool/Application/FrontEnd/ClientApp/src/pages/Login.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from 'react';
+
+export default function Login() {
+
+    return (
+        <h1>Hello World!</h1>
+    );
+
+}
