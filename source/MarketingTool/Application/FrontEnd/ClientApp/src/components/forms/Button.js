d371041145f41cb5d332b4f7cc9d21960380329d
Alex <acooupe@gmail.com>
2021-01-07 17:42:16 +0000

    Big reshuffle of structure and now using Blazor for UI rather than React

--- source/MarketingTool/Application/FrontEnd/ClientApp/src/components/forms/Button.js
+++
@@ -1,9 +0,0 @@
-﻿import React from 'react';
-
-export default function Button(props) {
-    return (
-        <div>
-            <button type={props.type} className={props.className} name={props.name} onClick={props.onClick} style={props.style}>{props.display}{props.children}</button>
-        </div>
-    )
-}
