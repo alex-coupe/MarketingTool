ac719fc703ce65e1719ca14db63b6ed0bd8bbbf0
Alex <acooupe@gmail.com>
2021-01-09 21:06:54 +0000

    Background service will now be a service api utilising DB triggers and signalR

---
+++ source/MarketingTool/ServiceApi/Controllers/PasswordResetController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PasswordResetController : ControllerBase
+    {
+    }
+}
