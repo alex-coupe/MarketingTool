db1dab03671ab3783335cd94ee6edeea65f648d7
Alex <acooupe@gmail.com>
2021-01-10 13:06:45 +0000

    Foundation for password reset service

---
+++ source/MarketingTool/ApiServices/PasswordResetService/PasswordResetService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiServices.PasswordResetService
+{
+    public class PasswordResetService : IService
+    {
+        public Task Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
