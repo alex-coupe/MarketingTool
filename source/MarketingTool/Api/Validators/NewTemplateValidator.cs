bb9f6e17462b664737166b2bfb93a7fcfd32fd95
Alex <acooupe@gmail.com>
2021-01-12 14:12:34 +0000

    Template validators

--- source/MarketingTool/Api/Validators/NewTemplateValidator.cs
+++ source/MarketingTool/Api/Validators/NewTemplateValidator.cs
@@ -10,7 +10,17 @@ namespace Api.Validators
     public class NewTemplateValidator : AbstractValidator<Template>
     {
         public NewTemplateValidator()
+            : base()
         {
+
+            RuleFor(template => template.Id).NotEmpty().GreaterThan(0).WithMessage("Id must be more than 0");
+            RuleFor(template => template.ClientId).NotEmpty().GreaterThan(0).WithMessage("Client Id must be more than 0");
+            RuleFor(template => template.CreatedDate).NotNull().WithMessage("Created date is required");
+            RuleFor(template => template.Name).NotNull().NotEmpty().WithMessage("Name is required");
+            RuleFor(template => template.Content).NotNull().NotEmpty().WithMessage("Content is required");
+            RuleFor(template => template.CreatorId).NotEmpty().GreaterThan(0).WithMessage("Creator Id must be more than 0");
+            RuleFor(template => template.Protected).NotNull().WithMessage("Protected can't be null");
+            RuleFor(template => template.Version).NotEmpty().GreaterThan(0).WithMessage("Version must be more than 0");
 
         }
     }
