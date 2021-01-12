2dbe26bf369580cecf4f09a2ee64114728128e31
Alex <acooupe@gmail.com>
2021-01-12 14:26:05 +0000

    Add client Id to templates table migration

---
+++ source/MarketingTool/DataAccess/Migrations/20210112142336_AddClientIdToTemplatesTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class AddClientIdToTemplatesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Templates",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Templates");
+        }
+    }
+}
