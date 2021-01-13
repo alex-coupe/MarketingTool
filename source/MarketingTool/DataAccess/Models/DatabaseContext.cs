ff2445858ed21630e28bdd23fbb2979d640aba8a
Alex <acooupe@gmail.com>
2021-01-13 19:43:49 +0000

    Setting up timesteps for when emails can be sent

--- source/MarketingTool/DataAccess/Models/DatabaseContext.cs
+++ source/MarketingTool/DataAccess/Models/DatabaseContext.cs
@@ -10,7 +10,19 @@ namespace DataAccess.Models
 
         }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<TimeStep>().HasData(new TimeStep { Id = 1, Name = "ASAP", Hours = 0 }, new TimeStep { Id = 2, Name = "Hourly", Hours = 1 }, new TimeStep { Id = 3, Name = "Daily", Hours = 24 }
+            , new TimeStep { Id = 4, Name = "Weekly", Hours = 168 }, new TimeStep { Id = 5, Name = "Bi-Weekly", Hours = 336 }, new TimeStep { Id = 6, Name = "4 Weekly", Hours = 672 }, new TimeStep
+            {
+                Id = 7,
+                Name = "Monthly",
+                Hours = 730
+            });
+        }
 
+
+        public DbSet<TimeStep> TimeSteps { get; set; }
         public DbSet<SubscriptionLevel> SubscriptionLevels { get; set; }
 
         public DbSet<Client> Clients { get; set; }
