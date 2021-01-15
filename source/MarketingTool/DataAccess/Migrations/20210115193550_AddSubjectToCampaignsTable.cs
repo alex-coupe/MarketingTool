067f9cac9f4f9934e591a608c6017acc699e510b
Alex <acooupe@gmail.com>
2021-01-15 19:36:50 +0000

    Migrations for previous commit

---
+++ source/MarketingTool/DataAccess/Migrations/20210115193550_AddSubjectToCampaignsTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class AddSubjectToCampaignsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Templates",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Templates");
+        }
+    }
+}
