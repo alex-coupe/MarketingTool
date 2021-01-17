0518d1cf8064a8a4259a87b7abfc0755b4ef8b9f
Alex <acooupe@gmail.com>
2021-01-17 16:38:42 +0000

    Template helper created to turn synonyms into values

---
+++ source/MarketingTool/Api/Helpers/TemplateHelper.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Models;
+using DataAccess.Repositories;
+using System.Linq;
+
+namespace Api.Helpers
+{
+    public static class TemplateHelper
+    {
+        
+        public static string ConvertSynonymsToValues(Template template, IRepository<TemplateSynonym> _synonymsRepository)
+        {
+            var finalTemplate = string.Empty;
+            var synonyms = _synonymsRepository.Where(x => x.ClientId == template.ClientId).ToList();
+
+
+            return finalTemplate;
+        }
+    }
+}
